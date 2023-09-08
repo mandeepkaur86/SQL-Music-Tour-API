d6f7b44438e0ea0de9378a9fc41907a94c399653
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-09-07 18:27:11 -0700

    add  migration file

--- models/band.js
+++ models/band.js
@@ -1,41 +1,46 @@
-const { INTEGER } = require("sequelize")
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
+    band_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    name:{
+       type:DataTypes.STRING,
+        allowNull:false
+    },
+    genre:{
+        type :DataTypes.TEXT,
+         allowNull:false
+    },
+    available_start_time:{
+        type:DataTypes.DATE,
+        allowNull:false
+    },
+    end_time: {
+       type:DataTypes.DATE,
+       allowNull:false,
+    }
 
-//DEPENDENCIES
-const {Sequelize,DataType,Model} = Require('sequeliz')
-const sequelize = new Sequelize(process.env.PG_URI)
-
-//MODEL CREATE
-class Band extends Model{ }
-
-Band.init({
-    band_id: { 
-        type: DataTypes.INTEGER, 
-        primaryKey: true, 
-        autoIncrement: true
-    },
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
-    },
-    genre: { 
-        type: DataTypes.TEXT, 
-        allowNull: false 
-    },
-    available_start_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-    end_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-}, {
+  }, {
     sequelize,
     modelName: 'Band',
-    tableName: 'band',
+    tableName: 'bands',
     timestamps:false
-}) 
-
-
-//EXPORT
-module. exports = Band
+  });
+  return Band;
+};
